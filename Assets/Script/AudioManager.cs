657027208ae4f0e8a64755bf5d3f4d70458bfd25
angiemc98 <molinanatalia@hotmail.com>
2024-08-01 00:51:22 -0500

    Audio principal ok.
    Pendiente añadir los SFX

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+   public Sound[] musicSounds, sfxSounds;
+   public AudioSource musicSource, sfxSource;
+
+   public AudioClip Theme;
+    public AudioClip Death;
+    public AudioClip Jump;
+    public AudioClip Win;
+
+
+
+    private void Awake() {
+
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start() 
+    {
+        musicSource.clip = Theme;
+        musicSource.Play();
+    }
+
+    public void PlayMusic(string name)
+    {
+          Sound s = Array.Find(musicSounds, (s) => s.name == name);
+
+          if (s != null)
+          {
+            Debug.Log("Sound not found");
+          }
+          else 
+          {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+          }
+    }
+
+    
+    public void PlaySFX(AudioClip clip)
+    {
+         sfxSource.PlayOneShot(clip);
+    }
+ 
+ 
+
+}
